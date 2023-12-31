4d32794c8ddcf4688a91804c7551f2667173a6f2
Hayatunnabi Nabil <nabilahmednafiahmed753@gmail.com>
2023-12-31 16:13:06 +0600

    Focus Admin Panel

--- resources/views/backend/partials/header.blade.php
+++ resources/views/backend/partials/header.blade.php
@@ -12,7 +12,7 @@
                 <div class="float-right">
                     <ul>
 
-                        <li class="header-icon dib"><i class="ti-bell"></i>
+                        {{--  <li class="header-icon dib"><i class="ti-bell"></i>
                             <div class="drop-down">
                                 <div class="dropdown-content-heading">
                                     <span class="text-left">Recent Notifications</span>
@@ -142,22 +142,22 @@
                                     </ul>
                                 </div>
                             </div>
-                        </li>
+                        </li>  --}}
                         <li class="header-icon dib"><span class="user-avatar">John <i
                                     class="ti-angle-down f-s-10"></i></span>
                             <div class="drop-down dropdown-profile">
-                                <div class="dropdown-content-heading">
+                                {{--  <div class="dropdown-content-heading">
                                     <span class="text-left">Upgrade Now</span>
                                     <p class="trial-day">30 Days Trail</p>
-                                </div>
+                                </div>  --}}
                                 <div class="dropdown-content-body">
                                     <ul>
-                                        <li><a href="#"><i class="ti-user"></i> <span>Profile</span></a></li>
+                                        {{--  <li><a href="#"><i class="ti-user"></i> <span>Profile</span></a></li>
 
                                         <li><a href="#"><i class="ti-email"></i> <span>Inbox</span></a></li>
                                         <li><a href="#"><i class="ti-settings"></i> <span>Setting</span></a></li>
 
-                                        <li><a href="#"><i class="ti-lock"></i> <span>Lock Screen</span></a></li>
+                                        <li><a href="#"><i class="ti-lock"></i> <span>Lock Screen</span></a></li>  --}}
                                         <li><a href="#"><i class="ti-power-off"></i> <span>Logout</span></a></li>
                                     </ul>
                                 </div>
