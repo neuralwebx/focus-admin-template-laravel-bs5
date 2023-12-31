58c4608cbe21be24b81e8aab0a955b248e9fa9c2
Hayatunnabi Nabil <nabilahmednafiahmed753@gmail.com>
2023-12-31 16:17:33 +0600

    Focus Admin Panel

--- resources/views/backend/partials/sidebar.blade.php
+++ resources/views/backend/partials/sidebar.blade.php
@@ -6,16 +6,15 @@
                         <!-- <img src="assets/images/logo.png" alt="" /> --><span>Admin Panel</span>
                     </a></div>
                 <li class="label">Main</li>
-                <li class="active"><a class="sidebar-sub-toggle"><i class="ti-home"></i> Dashboard <span
-                            class="badge badge-primary">2</span> <span
+                <li class="active"><a class="sidebar-sub-toggle"><i class="ti-home"></i> Dashboard  <span
                             class="sidebar-collapse-icon ti-angle-down"></span></a>
                     <ul>
                         <li><a href="{{ route('mstr') }}">Dashboard 1</a></li>
-                        {{--  <li><a href="index1.html">Dashboard 2</a></li>  --}}
+                        {{-- <li><a href="index1.html">Dashboard 2</a></li> --}}
                     </ul>
                 </li>
 
-                {{--  <li class="label">Apps</li>
+                {{-- <li class="label">Apps</li>
                 <li><a class="sidebar-sub-toggle"><i class="ti-bar-chart-alt"></i> Charts <span
                             class="sidebar-collapse-icon ti-angle-down"></span></a>
                     <ul>
@@ -27,7 +26,7 @@
                         <li><a href="chart-sparkline.html">Sparkle</a></li>
                         <li><a href="chart-knob.html">Knob</a></li>
                     </ul>
-                </li>  --}}
+                </li> --}}
             </ul>
         </div>
     </div>
